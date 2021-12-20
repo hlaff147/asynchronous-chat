1ebf40ce35be8380317307296f7293fe88e852bc
artn-dev <azemarteixeira@gmail.com>
2021-12-20 15:24:36 -0300

    make chat screen scrollable

--- message_screen.py
+++ message_screen.py
@@ -5,6 +5,8 @@ import imageio
 from threading import Thread
 from playsound import playsound
 
+from tmp import VerticalScrolledFrame
+
 
 class MessageScreen(Frame):
     def __init__(self, master, *args, **kwargs):
@@ -12,18 +14,26 @@ class MessageScreen(Frame):
         self.pack_propagate(False)
         self.bg = kwargs['bg']
 
+        self.frame = VerticalScrolledFrame(self, bg=self.bg)
+        self.frame.pack(fill=BOTH)
+
+    @property
+    def viewport(self):
+        return self.frame.interior
+
     def display_text(self, text):
-        Label(self, text=text, bg=self.bg, justify=LEFT, anchor='w').pack(fill=X)
+        Label(self.viewport, text=text, bg=self.bg, justify=LEFT, anchor='w').pack(fill=X)
 
     def clear(self):
-        for child in self.winfo_children():
+        for child in self.viewport.winfo_children():
             child.destroy()
+        self.frame.canvas.yview_moveto(0)
 
     def display_image(self, filename):
         img = Image.open(filename)
         img = img.resize((100, 100))
         self.photo = ImageTk.PhotoImage(img)
-        Label(self,
+        Label(self.viewport,
               image=self.photo,
               bg=self.bg,
               justify=LEFT,
@@ -41,7 +51,7 @@ class MessageScreen(Frame):
     def display_video(self, filename):
         self.video = imageio.get_reader(filename)
         delay = int(1000 / self.video.get_meta_data()['fps'])
-        self.l = Label(self,
+        self.l = Label(self.viewport,
               bg=self.bg,
               justify=LEFT,
               anchor='w'
@@ -50,7 +60,7 @@ class MessageScreen(Frame):
         Thread(target=self.stream, daemon=True).start()
 
     def display_audio(self, filename):
-        play_label = Label(self,
+        play_label = Label(self.viewport,
                            bg=self.bg,
                            justify=LEFT,
                            anchor='w'
